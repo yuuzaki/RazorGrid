72ec33111b2767cfe607d16bc742534275a3e79b
Yuuzaki <kira.yamato173@gmail.com>
2026-05-16 22:15:14 +0700

    add login2 with image panel, update boot strap version

--- BlazorGrid/Services/QueryParamProtector.cs
+++ BlazorGrid/Services/QueryParamProtector.cs
@@ -4,17 +4,17 @@ namespace BlazorGrid.Services;
 
 public class QueryParamProtector
 {
-    private readonly IDataProtector _protector;
+    private readonly ITimeLimitedDataProtector  _protector;
 
     public QueryParamProtector(IDataProtectionProvider provider)
     {
-        _protector = provider.CreateProtector("AuthFlow.QueryParams");
+        _protector = provider.CreateProtector("AuthFlow.QueryParams").ToTimeLimitedDataProtector();
     }
 
     public string Protect(string username, string redirectUrl)
     {
         var payload = $"{username}|{redirectUrl}";
-        return _protector.Protect(payload);
+        return _protector.Protect(payload, TimeSpan.FromSeconds(20));
     }
 
     public bool TryUnprotect(string? token, out string username, out string redirectUrl)
